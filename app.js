7c84fcd44f977eecb3436d8f0127bfe1ee9e9502
bhavyakukkar <nigelwithrow78@gmail.com>
2022-08-25 14:38:25 +0530

    64

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', (req, res, next) => {
     next();
 });
 
-app.use('/one', (req, res, next) => {
-    res.send('<h1>one</h1>');
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+    <form action="/product" method="POST">
+        <input type="text" name="title">
+        <button type="submit">Add Product</button>
+    </form>`);
 });
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
 app.use('/', (req, res, next) => {
     res.send('<h1>hhh</h1>');
