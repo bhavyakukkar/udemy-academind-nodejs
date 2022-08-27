5afb4601ad2838737efcea9791c6591f4cc7571d
bhavyakukkar <nigelwithrow78@gmail.com>
2022-08-27 21:20:47 +0530

    71

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -5,6 +7,7 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { dirname } = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -12,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Not Found</h1>");
+    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
 });
 
 app.listen(3000);
