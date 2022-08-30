34822f5887e653ace7140f2dbeb23be09ef0cab5
bhavyakukkar <nigelwithrow78@gmail.com>
2022-08-30 20:43:12 +0530

    79

--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path.js');
+const adminData = require('./admin').products;
 
 const router = express.Router();
 
+
 router.get('/', (req, res, next) => {
+    console.log(adminData);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
