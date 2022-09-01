db2e250cb39b2cae74689797d198cefe21c25d32
bhavyakukkar <nigelwithrow78@gmail.com>
2022-09-01 17:24:11 +0530

    101

--- models/product.js
+++ models/product.js
@@ -7,11 +7,24 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products');
-        products.push(this);
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if(!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => console.log(err));
+        });
     }
 
     static fetchAll() {
-        return products;
+        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return [];
+            }
+            return JSON.parse(fileContent);
+        });
     }
 }
