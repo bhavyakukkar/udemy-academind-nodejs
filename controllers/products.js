790343a4f97853f026aa479793ab8d5004b2d972
bhavyakukkar <nigelwithrow78@gmail.com>
2022-09-01 17:02:02 +0530

    100

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,9 @@
-const products = [];
-
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
         pageTitle: "Add Product",
-        path: "/admin/add-product",
+        path: req.path,
         activeAddProduct: true,
         formsCss: true,
         productCss: true
@@ -12,15 +11,17 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
         prods: products,
         pageTitle: "Shop",
-        path: '/',
+        path: req.path,
         hasProducts: products.length > 0,
         activeShop: true,
         productCss: true
