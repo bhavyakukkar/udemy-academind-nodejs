d79d7ba1f8fd7bf4948b333588ac86a68f1b13e3
bhavyakukkar <nigelwithrow78@gmail.com>
2022-09-06 21:53:12 +0530

    130

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,9 +33,21 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(productInCart => productInCart.id === product.id);
+                if(cartProductData) {
+                    cartProducts.push({productData: product, quantity: cartProductData.quantity});
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        })
     });
 };
 
